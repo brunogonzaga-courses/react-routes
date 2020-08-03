503b7f66a5015adafdef392cab55ad9c9fd359a5
Bruno <brunog86@gmail.com>
2020-08-02 23:18:11 -0300

    Fix issues em path and url in Courses Component and Passing data via props

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,9 +1,8 @@
 import React from 'react';
-
-import HTML from './courses/HTML';
-import CSS from './courses/CSS';
-import JavaScript from './courses/JavaScript';
 import { NavLink, Route, Redirect } from 'react-router-dom';
+
+import CourseContainer from './courses/CourseContainer';
+import { HTMLCourses, CSSCourses, JSCourses } from '../data/courses';
 
 
 const Courses = ({match}) => (
@@ -11,17 +10,17 @@ const Courses = ({match}) => (
     <div className="course-header group">
       <h2>Courses</h2> 
       <ul className="course-nav">
-        <li><NavLink to={`${match}/html`} >HTML</NavLink></li>
-        <li><NavLink to={`${match}/css`} >CSS</NavLink></li>
-        <li><NavLink to={`${match}/javascript`} >JavaScript</NavLink></li>
+        <li><NavLink to={`${match.url}/html`} >HTML</NavLink></li>
+        <li><NavLink to={`${match.url}/css`} >CSS</NavLink></li>
+        <li><NavLink to={`${match.url}/javascript`} >JavaScript</NavLink></li>
       </ul>
     </div>
     
     {/* Write routes here... */}
-    <Route exact path={match.path} render={ () => <Redirect to={`${match}/html`} /> } />
-    <Route path={`${match}/html`} component={HTML} />
-    <Route path={`${match}/css`} component={CSS} />
-    <Route path={`${match}/javascript`} component={JavaScript} />
+    <Route exact path={match.path} render={ () => <Redirect to={`${match.path}/html`} /> } />
+    <Route path={`${match.path}/html`} render={ () => <CourseContainer data={HTMLCourses} /> } />
+    <Route path={`${match.path}/css`} render={ () => <CourseContainer data={CSSCourses} /> } />
+    <Route path={`${match.path}/javascript`} render={ () => <CourseContainer data={JSCourses} /> } />
   </div>
 );
 
